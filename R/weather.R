f2547cfa97c94d80b9d40dd7028ff8e2399a8054
hadley <h.wickham@gmail.com>
2014-06-27 14:33:38 -0500

    Add weather data

---
+++ R/weather.R
@@ -0,0 +1,20 @@
+#' Hourly weather data
+#'
+#' Hourly meterological data for LGA, JFK and EWR.
+#'
+#' @source ASOS download from Iowa Environmental Mesonet,
+#'   https://mesonet.agron.iastate.edu/request/download.phtml.
+#' @format A data frame with columns
+#' \describe{
+#' \item{origin}{Weather station. Named origin to faciliate merging with
+#'   \code{\link{flights}} data}
+#' \item{year,month,day,hour}{Time of recording}
+#' \item{temp,dewp}{Temperature and dewpoint in F}
+#' \item{humid}{Relative humidity}
+#' \item{wind_dir,wind_speed,wind_guest}{Wind direction (in degrees), speed
+#'   and gust speed (in mph)}
+#' \item{precip}{Preciptation, in inches}
+#' \item{pressure}{Sea level pressure in millibars}
+#' \item{visib}{Visibility in miles}
+#' }
+"weather"
---
+++ data-raw/weather.R
@@ -0,0 +1,57 @@
+# From https://mesonet.agron.iastate.edu/request/download.phtml?network=NY_ASOS
+
+library(httr)
+library(dplyr)
+library(lubridate)
+
+# Download ---------------------------------------------------------------------
+
+get_asos <- function(station) {
+  url <- "https://mesonet.agron.iastate.edu/cgi-bin/request/getData.py"
+  query <- list(
+    station = station, data = "all",
+    year1 = "2013", month1 = "1", day1 = "1",
+    year2 = "2013", month2 = "12", day2 = "31", tz = "GMT",
+    format = "comma", latlon = "no", direct = "yes")
+
+  r <- GET(url, query = query)
+  stop_for_status(r)
+
+  writeBin(content(r, "raw"), paste0("data-raw/weather/", station, ".csv"))
+}
+stations <- c("JFK", "LGA", "EWR")
+paths <- paste0(stations, ".csv")
+missing <- stations[!(paths %in% dir("data-raw/weather/"))]
+lapply(missing, get_asos)
+
+# Load ------------------------------------------------------------------------
+
+read.csv("data-raw/weather/EWR.csv", ) %>% tbl_df()
+
+paths <- dir("data-raw/weather/", full.names = TRUE)
+all <- lapply(paths, read.csv, skip = 4, row.names = NULL, check.names = FALSE,
+  header = FALSE, stringsAsFactors = FALSE, na.strings = "M")
+
+raw <- rbind_all(all)
+names(raw) <- c("station", "time", "tmpf", "dwpf", "relh", "drct", "sknt",
+  "p01i", "alti", "mslp", "vsby", "gust", "skyc1", "skyc2", "skyc3", "skyc4",
+  "skyl1", "skyl2", "skyl3", "skyl4", "metar")
+
+weather <- raw %>%
+  tbl_df() %>%
+  select(
+    station, time, temp = tmpf, dewp = dwpf, humid = relh,
+    wind_dir = drct, wind_speed = sknt, wind_gust = gust,
+    precip = p01i, pressure = mslp, visib = vsby
+  ) %>%
+  mutate(
+    time = as.POSIXct(strptime(time, "%Y-%m-%d %H:%M")),
+    wind_speed = as.numeric(wind_speed) * 1.15078, # convert to mpg
+    wind_gust = as.numeric(wind_speed) * 1.15078
+  ) %>%
+  mutate(year = 2013, month = month(time), day = mday(time), hour = hour(time)) %>%
+  group_by(month, day, hour) %>%
+  filter(row_number() == 1) %>%
+  select(origin = station, year:hour, temp:visib)
+
+save(weather, file = "data/weather.rda")
