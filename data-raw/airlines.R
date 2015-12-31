f6eff85a5c1d831dad78b7de4b8ea8793ecd1872
hadley <h.wickham@gmail.com>
2015-12-31 08:22:33 -0600

    Cache processed airlines csv for easier diffing

--- data-raw/airlines.R
+++ data-raw/airlines.R
@@ -1,19 +1,14 @@
 library(dplyr)
 library(readr)
 
-src <- "http://www.transtats.bts.gov/Download_Lookup.asp?Lookup=L_UNIQUE_CARRIERS"
-lcl <- "data-raw/airlines.csv"
-
-if (!file.exists(lcl)) {
-  download.file(src, lcl)
-}
+raw <- read_csv("http://www.transtats.bts.gov/Download_Lookup.asp?Lookup=L_UNIQUE_CARRIERS")
 
 load("data/flights.rda")
 
-raw <- read_csv(lcl)
 airlines <- raw %>%
   select(carrier = Code, name = Description) %>%
   semi_join(flights) %>%
   arrange(carrier)
 
+write_csv(airlines, "data-raw/airlines.csv")
 save(airlines, file = "data/airlines.rda")
