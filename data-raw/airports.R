bee6b5d70020cc3cbbe4c6a25af695a7add2f986
hadley <h.wickham@gmail.com>
2016-12-29 08:55:32 -0600

    Save airports as svg for better diff
    Use theme_void() to minimise spurious changes

--- data-raw/airports.R
+++ data-raw/airports.R
@@ -25,7 +25,9 @@ airports %>%
   filter(lon < 0) %>%
   ggplot(aes(lon, lat)) +
   geom_point(aes(colour = factor(tzone)), show.legend = FALSE) +
-  coord_quickmap()
+  coord_quickmap() +
+  theme_void()
+ggsave("data-raw/airports.svg", width = 8, height = 6)
 
 write_csv(airports, "data-raw/airports.csv")
 save(airports, file = "data/airports.rda")
