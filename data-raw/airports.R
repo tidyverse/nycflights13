1fe36057ef684196a627f6ae7e956061a4b01277
Wlodek Bzyl <matwb@ug.edu.pl>
2016-12-29 15:51:09 +0100

    Airports data moved to GitHub (#18)
    * update airports.R
    
    * regenerate airports.{csv,dat,rda}
    
    * add airports.png

--- data-raw/airports.R
+++ data-raw/airports.R
@@ -1,39 +1,23 @@
 library(dplyr)
 library(readr)
 library(purrr)
-library(geonames) # to determine timezones
 
 if (!file.exists("data-raw/airports.dat")) {
   download.file(
-    "http://sourceforge.net/p/openflights/code/HEAD/tree/openflights/data/airports.dat?format=raw",
+    "https://raw.githubusercontent.com/jpatokal/openflights/master/data/airports.dat",
     "data-raw/airports.dat"
   )
 }
 
 raw <- read_csv("data-raw/airports.dat",
-  col_names = c("id", "name", "city", "country", "faa", "icao", "lat", "lon", "alt", "tz", "dst")
+  col_names = c("id", "name", "city", "country", "faa", "icao", "lat", "lon", "alt", "tz", "dst", "tzone")
 )
 
 airports <- raw %>%
   filter(country == "United States", faa != "") %>%
-  select(faa, name, lat, lon, alt, tz, dst) %>%
+  select(faa, name, lat, lon, alt, tz, dst, tzone) %>%
   group_by(faa) %>% slice(1) %>% ungroup() # take first if duplicated
 
-
-# Find time zones
-get_tz <- function(lat, lon) {
-  cat(".")
-  GNtimezone(lat, lon)
-}
-tz <- map2(airports$lat, airports$lon, safely(get_tz))
-tz <- transpose(tz)
-
-ok <- tz$error %>% map_lgl(is.null)
-airports[!ok, ]
-
-airports$tzone <- tz$result %>%
-  map("timezoneId", .null = NA) %>%
-  map_chr(as.character)
 
 # Verify the results
 library(ggplot2)
