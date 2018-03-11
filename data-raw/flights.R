f3eaee9d9d8d7536c2221b75ade907d17bbd7b38
Ian Cook <ianmcook@gmail.com>
2018-03-11 12:03:56 -0400

    Update BTS download URL in flights.R

--- data-raw/flights.R
+++ data-raw/flights.R
@@ -2,7 +2,7 @@ library(dplyr)
 library(readr)
 
 flight_url <- function(year = 2013, month) {
-  base_url <- "http://www.transtats.bts.gov/Download/"
+  base_url <- "http://www.transtats.bts.gov/PREZIP/"
   sprintf(paste0(base_url, "On_Time_On_Time_Performance_%d_%d.zip"), year, month)
 }
 
