e2de1f8b6c3298a1a2b4cc3688c87f26e7b20e63
Ben Baumer <ben.baumer@gmail.com>
2019-03-12 17:03:13 -0400

    fix for new URL; close #59

--- R/etl_extract.R
+++ R/etl_extract.R
@@ -9,10 +9,16 @@
 etl_extract.etl_airlines <- function(obj, years = 2015, months = 1:12, ...) {
   
   valid_months <- valid_year_month(years, months, begin = "1987-10-01") %>%
-    mutate_(url = ~paste0("http://transtats.bts.gov/PREZIP/On_Time_On_Time_Performance_", 
-                     year, "_", month, ".zip"))
+    dplyr::mutate(
+      url = paste0("http://transtats.bts.gov/PREZIP/On_Time_Reporting_Carrier_On_Time_Performance_1987_present_", 
+                   year, "_", month, ".zip"), 
+      filename = gsub("Reporting_Carrier_", "", url), 
+      filename = gsub("1987_present_", "", filename),
+      filename = basename(filename)
+    )
   
   message("Downloading flight data...")
-  etl::smart_download(obj, valid_months$url)
+  etl::smart_download(obj, valid_months$url, 
+                      new_filenames = valid_months$filename)
   invisible(obj)
 }
