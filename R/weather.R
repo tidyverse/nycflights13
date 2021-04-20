0eb144bce93b0f57235de444a607f5eebe397a24
Sean Kross <smk240@gmail.com>
2021-04-20 06:09:00 -0700

    fixed two typos (#43)

--- R/weather.R
+++ R/weather.R
@@ -1,6 +1,6 @@
 #' Hourly weather data
 #'
-#' Hourly meterological data for LGA, JFK and EWR.
+#' Hourly meteorological data for LGA, JFK and EWR.
 #'
 #' @source ASOS download from Iowa Environmental Mesonet,
 #'   <https://mesonet.agron.iastate.edu/request/download.phtml>.
