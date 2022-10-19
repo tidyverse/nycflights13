2fe0ae6fbb87d07f6f7edc15a2f5cfb3599b72bd
guslipkin <gus.lipkin@gmail.com>
2022-10-19 15:18:32 -0400

    Fixes the documentation in #47
    This does not fix the underlying functions in data-raw

--- R/airlines.R
+++ R/airlines.R
@@ -2,7 +2,7 @@
 #'
 #' Look up airline names from their carrier codes.
 #'
-#' @source <https://www.transtats.bts.gov/DL_SelectFields.asp?Table_ID=236>
+#' @source <https://www.transtats.bts.gov/DL_SelectFields.aspx?gnoyr_VQ=FGJ&QO_fu146_anzr=b0-gvzr>
 #' @format Data frame with columns
 #' \describe{
 #' \item{carrier}{Two letter abbreviation.}
